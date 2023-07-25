16263520e03d99c19baeeaeba92568fa473f30a0
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-25 18:56:17 +0530

    customformat-winston-logger
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- express-sequelize-logging/index.js
+++ express-sequelize-logging/index.js
@@ -1,22 +1,30 @@
 const sequelize = require('./src/config/database');
 const app = require('./src/app');
 const UserService = require('./src/user/UserService');
+const logger = require('./src/logger');
 
-if(process.env.NODE_ENV === 'production') {
-  sequelize.sync();
+if (process.env.NODE_ENV === 'production') {
+	sequelize.sync();
 } else {
-  sequelize.sync({ force: true }).then(async () => {
-    for(let i = 1; i <= 5; i++) {
-      const user = {
-        username: `user${i}`,
-        email: `user${i}@mail.com`,
-        password: 'P4ssword'
-      }
-      await UserService.create(user);
-    }
-  });
+	sequelize.sync({ force: true }).then(async () => {
+		for (let i = 1; i <= 5; i++) {
+			const user = {
+				username: `user${i}`,
+				email: `user${i}@mail.com`,
+				password: 'P4ssword',
+			};
+			await UserService.create(user);
+		}
+	});
 }
 
+logger.error('Error');
+logger.warn('Warn');
+logger.info('Info');
+logger.verbose('Verbose');
+logger.debug('Debug');
+logger.silly('Silly');
+
 app.listen(process.env.PORT || 3000, () => {
-  console.log("app is running in mode: ", process.env.NODE_ENV);
+	logger.info('app is running in mode: ', process.env.NODE_ENV);
 });
--- express-sequelize-logging/src/config/database.js
+++ express-sequelize-logging/src/config/database.js
@@ -1,12 +1,21 @@
 const { Sequelize } = require('sequelize');
 const config = require('config');
+const logger = require('../logger');
 
-const configuration = config.get("database");
+const configuration = config.get('database');
 
-const sequelize = new Sequelize(configuration.db, configuration.username, configuration.password, {
-  dialect: configuration.dialect,
-  host: configuration.host,
-  logging: configuration.logging
-})
+const sequelize = new Sequelize(
+	configuration.db,
+	configuration.username,
+	configuration.password,
+	{
+		dialect: configuration.dialect,
+		host: configuration.host,
+		//logging: configuration.logging
+		logging: (message) => {
+			logger.info(message);
+		},
+	},
+);
 
 module.exports = sequelize;
